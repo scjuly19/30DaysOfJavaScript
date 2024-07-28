ed4aaacf7a3741aa23f8a1d978a40707d2299009
scjuly19 <scjuly69@gmail.com>
2024-07-28 12:45:00 +0530

    fix: Fix logic for minutes and hour

--- Day2_Clock/index.js
+++ Day2_Clock/index.js
@@ -4,10 +4,24 @@ const hrsHand = document.querySelector(".hour")
 const timeInWords = document.querySelector('.time-in-words')
 function setDate() {
     const date = new Date();
+
     const seconds = date.getSeconds()
-    const minutes = date.getMinutes()
-    const hour = date.getHours()
     const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+    const minutes = date.getMinutes()
+    /**
+     * We multiply seconds/60 here with 6 because every 6 degrees represents 1 minute in a clock
+     */
+    const minsDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`
+    const hour = date.getHours()
+    /**
+     * We multiply minutes/60 here with 30 because every 30 degrees represents 1 hour in a clock
+     */
+    const hourDegrees = ((hour / 12) * 360) + ((minutes / 60) * 30) + 90;
+    hrsHand.style.transform = `rotate(${hourDegrees}deg)`
+
     if (seconds === 0) {
         secondsHand.style.transitionDuration = '0s';
         minsHand.style.transitionDuration = '0s';
@@ -19,12 +33,9 @@ function setDate() {
         minsHand.style.transitionDuration = '0.05s';
         hrsHand.style.transitionDuration = '0.05s';
     }
-    const minDegrees = (((minutes / 60) * 360) + 90)
-    const hrsDegrees = (((hour / 24) * 360) + 90)
-    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`
-    minsHand.style.transform = `rotate(${minDegrees}deg)`
-    hrsHand.style.transform = `rotate(${hrsDegrees}deg)`
+
     timeInWords.innerHTML = `${hour}:${minutes}:${seconds}`
 
 }
-setInterval(setDate, 1000)
+setInterval(setDate, 1000);
+setDate();
